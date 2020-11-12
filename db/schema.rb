4cdbfaef03a4fdfc434e51790f5e674b3f5633b5
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-11-12 14:17:19 -0600

    Added orders model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_193930) do
+ActiveRecord::Schema.define(version: 2020_11_12_201601) do
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "order_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
