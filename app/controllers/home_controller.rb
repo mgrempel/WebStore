a97e62c193b216801056edf7ea19bc218a0214a8
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-11-14 20:28:21 -0600

    finished 1.4 contact and about pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
-  def index
-  end
+  def index; end
 
   def about
+    @about = About.first
   end
 
   def contact
+    @contact = Contact.first
   end
 end
