d8a7d35eab05278e5296c360b582fb0fdb4b08d6
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-11-15 15:32:25 -0600

    1.6 added seeds script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+require "faker"
+
+AdminUser.destroy_all
+Category.destroy_all
+Item.destroy_all
+
+if Rails.env.development?
+  AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
+end
+
+4.times do
+  category = Category.create(name:        Faker::Commerce.unique.department,
+                             description: Faker::Company.unique.bs)
+  # populate category with products
+  25.times do
+    category.items.build(name:        Faker::Commerce.unique.product_name,
+                         description: Faker::Commerce.material,
+                         price:       Faker::Commerce.price)
+    category.save
+  end
+end
