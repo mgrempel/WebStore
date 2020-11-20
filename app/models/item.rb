413ee8e977ff28cfd0b49372645887a94893c383
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-11-20 09:40:56 -0600

    Minor fixes to seed script and markdown field

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,6 @@ class Item < ApplicationRecord
   validates :name, :description, :price, presence: true
   validates :name, length: { maximum: 30 }
   validates :description, length: { maximum: 500 }
-  validates :markdown, :price, numericality: { greater_than: 0 }
+  validates :markdown, :price, numericality: { greater_than_or_equal_to: 0 }
   validates :markdown, numericality: { less_than: :price }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,13 @@ end
 
 4.times do
   category = Category.create(name:        Faker::Commerce.unique.department,
-                             description: Faker::Company.unique.bs)
+                             description: Faker::Company.bs)
   # populate category with products
   25.times do
     category.items.build(name:        Faker::Commerce.unique.product_name,
                          description: Faker::Commerce.material,
-                         price:       Faker::Commerce.price)
+                         price:       Faker::Commerce.price,
+                         markdown:    0)
     category.save
   end
 end
