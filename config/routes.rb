869a6d4d6343e6f39c5f9b4c0c01b33b9003434f
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-11-21 14:58:32 -0600

    Added categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get "/categories/:id", to: "categories#show", as: "category"
   get "/items/:id", to: "items#show", as: "item"
-  get 'home/index'
-  get 'home/about'
-  get 'home/contact'
+  get "home/index"
+  get "home/about"
+  get "home/contact"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
