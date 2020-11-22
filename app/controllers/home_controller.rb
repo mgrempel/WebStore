363c35b91aba2d3896b22b3d5757a1c06e848b06
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-11-22 11:59:23 -0600

    Rendered search field on page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @search_options = ["Newest", "Recently Updated", "On Sale"]
+    @current_selection = (params[:option] || @search_options[0])
     @products = Item.all
     @categories = Category.all
   end
