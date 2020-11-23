4e30256885fb03a0c08900edda04b9b2e385c909
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-11-23 17:24:09 -0600

    Added cart controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'cart/view'
   get "/categories/:id", to: "categories#show", as: "category"
   get "/items/:id", to: "items#show", as: "item"
   get "home/index"
