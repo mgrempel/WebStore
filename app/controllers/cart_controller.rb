bc306c343e4acc9f41134fdfab5d4f1eea816001
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-11-23 20:10:43 -0600

    Implemented cart add functionality

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,19 @@
 class CartController < ApplicationController
   def view
+    @cart = cookies[:cart]
+  end
+
+  def add
+    cart = {}
+    cart = JSON.parse(cookies[:cart]) unless cookies[:cart].nil?
+
+    # Determine if we're creating or adding to the cart.
+    cart[params[:item_id]] = if cart[params[:item_id]].nil?
+                               params[:amount].to_i
+                             else
+                               cart[params[:item_id]].to_i + params[:amount].to_i
+                             end
+
+    cookies[:cart] = cart.to_json
   end
 end
