8b81aff35468af2ac1bfb3a45033e0ff623786e0
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-12-01 11:20:46 -0600

    Fixed create account bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,5 @@ class User < ApplicationRecord
   has_many :address_infos
   has_many :billing_infos
 
-  validates :username, :password_digest, presence: true
-  validates :username, length: { maximum: 30, minimum: 5 }
-  validates :password_digest, length: { maximum: 1000, minimum: 8 }
   validates :account_prefs, length: { maximum: 1000 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_161444) do
+ActiveRecord::Schema.define(version: 2020_12_01_171623) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "title"
@@ -142,7 +142,6 @@ ActiveRecord::Schema.define(version: 2020_12_01_161444) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
     t.string "account_prefs"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
