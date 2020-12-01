0b8e2864ec2d80160baf2ff87f4f167b4acb9aa0
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-12-01 12:34:52 -0600

    Added provinces model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_171623) do
+ActiveRecord::Schema.define(version: 2020_12_01_183425) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "title"
@@ -138,6 +138,15 @@ ActiveRecord::Schema.define(version: 2020_12_01_171623) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.decimal "gst"
+    t.decimal "pst"
+    t.decimal "hst"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
