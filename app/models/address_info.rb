f35b1b5b8f6b0c00fc833a1dd1af5a3fcc111c82
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-12-06 10:47:15 -0600

    Removed unnecessary billing_info model

--- app/models/address_info.rb
+++ app/models/address_info.rb
@@ -1,7 +1,7 @@
 class AddressInfo < ApplicationRecord
   belongs_to :province
-  has_one :billing_address_info, class_name: "AddressInfo", foreign_key: "billing_address_info_id"
-  has_one :shipping_address_info, class_name: "AddressInfo", foreign_key: "shipping_address_info_id"
+  # has_one :billing_address_info, class_name: "AddressInfo", foreign_key: "billing_address_info_id"
+  # has_one :shipping_address_info, class_name: "AddressInfo", foreign_key: "shipping_address_info_id"
   belongs_to :user
 
   # canadian_postal_code = /\A[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1}[ -]?\\d{1}[A-Z]{1}\\d{1}\z/
