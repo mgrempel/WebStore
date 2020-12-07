fddcec09b3daa6166f3ee4c5056d040cb19822ac
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-12-07 13:37:09 -0600

    Fixed order_item not associating with items

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :order, optional: true
-  belongs_to :items, optional: true
+  belongs_to :item, optional: true
 
   validates :item_price, presence: true
   validates :item_price, numericality: { greater_than: 0 }
